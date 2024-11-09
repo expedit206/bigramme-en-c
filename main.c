65a1d7662716c2bc7fc869909900d51e6a1d6d9a
Expedit206 <mciagnessi@gmail.com>
2024-11-09 20:43:48 +0100

    mon bigramme

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 int main()
 {
-
     char str[50], bigram[50];
     int i;
 
@@ -21,19 +20,18 @@ int main()
 }
 
 
-
 void bigramme(char nom[], char bigram[]){
 int i, nbr;
 char tab[strlen(nom)-1][2];
-for(i=0; i < strlen(nom); i++){
+
+for(i=0 ; i < strlen(nom) ; i++){
         tab[i][0] = nom[i];
         tab[i][1] = nom[i+1];
-
 }
 
 nbr = 0;
 for(i=0; i <= strlen(nom); i++){
-
+    
 if(tab[i][0]==bigram[0] && tab[i][1]==bigram[1]){
     nbr++;
 }
